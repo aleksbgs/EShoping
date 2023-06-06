d01eafa08e42bbb11ac7cd6cd6cf15aab7b482cb
Aleksandar Markovic <amarkovic29@gmail.com>
2023-06-06 12:30:54 +0200

    first commit

---
+++ Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProducts();
+    Task<Product> GetProduct(string id);
+    Task<IEnumerable<Product>> GetProductByName(string id);
+    Task<IEnumerable<Product>> GetProductByBrand(string id);
+    Task<Product> CreateProduct(Product product);
+    Task<bool> UpdateProduct(Product product);
+    Task<bool> DeleteProduct(string id);
+}
