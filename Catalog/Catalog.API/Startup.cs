7d9cd7bc7014a2e159a3ea2c2ae80dc976a7b0b3
Aleksandar Markovic <amarkovic29@gmail.com>
2023-06-24 16:41:19 +0200

    dsf

--- Catalog/Catalog.API/Startup.cs
+++ Catalog/Catalog.API/Startup.cs
@@ -1,19 +1,42 @@
+using System.Reflection;
+using Catalog.Application.Handlers;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using Catalog.Infrastructure.Repositories;
+using HealthChecks.UI.Client;
+using MediatR;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Catalog.API;
 
 public class Startup
 {
+    private readonly IConfiguration _configuration;
     public IConfiguration Configuration;
 
     public Startup(IConfiguration configuration)
     {
-        Configuration = configuration;
+        _configuration = configuration;
     }
 
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
+        services.AddApiVersioning();
+        services.AddHealthChecks()
+            .AddMongoDb(_configuration["Logging:ConnectionStrings:DatabaseName"], "Catalog  Mongo Db Health Check",
+                HealthStatus.Degraded);
+        services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "4Create.API", Version = "v1" }); });
+        //DI
+        services.AddAutoMapper(typeof(Startup));
+        services.AddMediatR(typeof(CreateProductHandler).GetTypeInfo().Assembly);
+        services.AddScoped<ICatalogContext, CatalogContext>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IBrandRepository, ProductRepository>();
+        services.AddScoped<ITypesRepository, ProductRepository>();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -21,14 +44,25 @@ public class Startup
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
         }
 
         app.UseRouting();
-        app.UseStaticFiles();
-        app.UseAuthorization();
+            app.UseStaticFiles();
+            app.UseAuthorization();
 
-        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+
+            });
+        }
     }
-}
 
 
