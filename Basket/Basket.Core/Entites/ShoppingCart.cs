11822f6d298a5ee95f0611a2c596c701f07691cd
Aleksandar Markovic <amarkovic29@gmail.com>
2023-06-25 21:47:27 +0200

    gft

--- Basket/Basket.Core/Entites/ShoppingCart.cs
+++ Basket/Basket.Core/Entites/ShoppingCart.cs
@@ -4,6 +4,7 @@ public class ShoppingCart
 {
 
     public string UserName { get; set; }
+    public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
 
     public ShoppingCart()
     {
